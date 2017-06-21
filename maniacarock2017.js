9b989a9ca89687c6e08020702842354269695568
MikiDani <free.ingyenes@gmail.com>
2017-06-21 19:04:36 +0200

    change encodeing

--- maniacarock2017.js
+++ maniacarock2017.js
@@ -1,18 +1,18 @@
 var startsite = new Site("start");
 
 startsite.reachable = true;
-startsite.description  = "hellÛ vil·g";
+startsite.description  = "hell√≥ vil√°g";
 startsite.addOption("bumm1", function(){printc(this.name);});
 startsite.addOption("bumm2", function(){printc(this.name);});
 
 var seconds = new Site("second");
 
 seconds.reachable = true;
-seconds.description  = "hellÛ m·sodik vil·g";
+seconds.description  = "hell√≥ m√°sodik vil√°g";
 seconds.addOption("nyit", function(){
 	thirds.reachable = true;
 	printc("third nyitva");
-	refresh_ui();
+	refresh();
 });
 
 newlontool = new Tool("newlontool");
@@ -21,8 +21,8 @@ newlontool.owned = true;
 var thirds = new Site("third");
 
 thirds.reachable = false;
-thirds.description  = "hellÛ haradik vil·g";
+thirds.description  = "hell√≥ haradik vil√°g";
 
 thirds.addToolRelay("newlontool", function(){
-	printc("nem, a ny˙l az!");
+	printc("nem, a ny√∫l az!");
 });
